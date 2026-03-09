3f37154f3ee02d6ef35f3a7957841f885353e9a9
Awilistrators <amar281299@gmail.com>
2026-03-09 09:55:17 +0700

    scan.js

--- scan.js
+++ scan.js
@@ -154,9 +154,6 @@ function simpan() {
 
   fetch(API_URL, {
   method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
   body: JSON.stringify(payload)
 })
 .then(r => r.json())
